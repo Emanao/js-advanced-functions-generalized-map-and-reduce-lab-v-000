7ac021b8b0a11ef0f38d4c3120f6fbf18d677cad
Eva Navarro <emanao@me.com>
2020-07-15 19:45:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 function map(ary, cb){
   const new_ary = []
-  ary.forEach(elem=>new_ary.push(cb(elem)));
+  ary.forEach(elem => new_ary.push(cb(elem)));
   return new_ary;
 }
 
 function reduce(ary, cb, initialValue){
   let accu =!!initialValue?initialValue:ary[0];
-  ary.forEach(elem=>accu = cb(elem, accu));
+  console.log(accu);
+  ary.forEach(elem => accu = cb(elem, accu));
   return accu;
 }
-
-// function reduceToTotal(ary,initialValue){
-//   let accu =!!initialValue?initialValue:0;
-//   ary.forEach(elem=>accu+=elem);
-//   return accu;
-// }
-// function reduceToAllTrue(ary){
-//   console.log(ary);
-//   let accu=true;
-//    ary.forEach(elem=> accu= accu&&Boolean(elem));
-//   return accu;
-// }
-// function reduceToAnyTrue(ary){
-//   let accu=false;
-//    ary.forEach(elem=> accu= accu||Boolean(elem));
-//   return accu;
-// }
