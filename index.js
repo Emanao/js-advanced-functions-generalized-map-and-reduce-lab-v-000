c25cacf2920585b06a4dbdde778a390f710dd3b4
Eva Navarro <emanao@me.com>
2020-07-15 19:39:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function map(ary, cb){
 
 function reduce(ary, cb, initialValue){
   console.log(arguments[1])
-  let accu;
+  let accu=0;
   ary.forEach(elem=>cb(accu, elem, initialValue));
   return accu;
 }
