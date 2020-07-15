2cd571021a3770bf37e3ad9c5be712f3dcb1e371
Eva Navarro <emanao@me.com>
2020-07-15 19:32:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ function map(ary, cb){
   ary.forEach(elem=>new_ary.push(cb(elem)));
   return new_ary;
 }
+
+function reduce(ary, cb, initialValue){
+  let accu =!!initialValue?initialValue:0;
+  ary.forEach(elem=>cb(accu,elem));
+  return accu;
+}
+
+// function reduceToTotal(ary,initialValue){
+//   let accu =!!initialValue?initialValue:0;
+//   ary.forEach(elem=>accu+=elem);
+//   return accu;
+// }
+// function reduceToAllTrue(ary){
+//   console.log(ary);
+//   let accu=true;
+//    ary.forEach(elem=> accu= accu&&Boolean(elem));
+//   return accu;
+// }
+// function reduceToAnyTrue(ary){
+//   let accu=false;
+//    ary.forEach(elem=> accu= accu||Boolean(elem));
+//   return accu;
+// }
